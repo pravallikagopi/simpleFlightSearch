d69e2ecf9cb9a7e59277fb9273c49c69c11f1109
pravallikagopi <pravallikagopi7@gmail.com>
2019-05-24 19:45:47 +0530

    Adding default route
    for default route displaying search component

--- flightSearchUI/src/app/app-routing.module.ts
+++ flightSearchUI/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
+  {path: '', component: SearchComponent},
   {path: 'search', component: SearchComponent}
 ];
 
